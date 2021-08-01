3eeddde5b5f897186022cb827d9095ef8a072fa7
summea <summea@gmail.com>
2021-07-31 22:19:10 -0700

    Added random button feature, repeat-one button feature, added unique id's to albums and songs, and addressed random button playback logic to not play song right when random button is pushed. Closes #31, closes #32, closes #33, closes #34.

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ let songList = document.getElementById('songList');
 let albumListOpen = false;
 let playButton = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"><path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/></svg>';
 let pauseButton = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16"><path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z"/></svg>';
+let randomButton = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-diamond" viewBox="0 0 16 16"><path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/></svg>';
 let data = [];
 let currentAudioLoaded = false;
+let randomButtonEnabled = false;
+let repeatOneButtonEnabled = false;
 
 window.onload = function() {
   // Get song data
@@ -30,7 +33,7 @@ window.onload = function() {
         let li = document.createElement('li');
         let link = document.createElement('a');
         let linkText = document.createTextNode(album.name);
-        link.id = i;
+        link.id = 'album_' + i;
         link.href = album.url;
         link.className = 'album';
         link.append(linkText);
@@ -59,6 +62,8 @@ let audioPlayerSlider = document.getElementById('audioPlayerSlider');
 let play = document.getElementById('play');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
+let random = document.getElementById('random');
+let repeatOne = document.getElementById('repeatOne');
 
 let playPromise;
 
@@ -108,10 +113,18 @@ function playbackTimeUpdate(playFromTime) {
 
 // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
 play.addEventListener('click', event => {
-  if (!albumListOpen) {
+  if (!albumListOpen && !randomButtonEnabled) {
     return false;
   }
-  audioPlayer.src = songs[currentSongIndex].url;
+  if (randomButtonEnabled) {
+    randomSong = getRandomSong();
+    audioPlayer.src = data['albums'][randomSong['albumId']]['songs'][randomSong['songId']].url;
+    currentAudioTitle.innerHTML = data['albums'][randomSong['albumId']]['songs'][randomSong['songId']].name;
+  } else {
+    audioPlayer.src = songs[currentSongIndex].url;
+    currentAudioTitle.innerHTML = songs[currentSongIndex].name;
+  }
+  // TODO: Might need to check this; random songs might not get played as expected
   if (!currentAudioLoaded) {
     currentAudioTitle.innerHTML = songs[currentSongIndex].name
         + '<br>' + 'Loading audio...';
@@ -135,7 +148,6 @@ play.addEventListener('click', event => {
     clearInterval(timeChecking);
     play.innerHTML = pauseButton;
   }
-  currentAudioTitle.innerHTML = songs[currentSongIndex].name;
 });
 
 // Get length of current song once song can be played without buffering
@@ -154,18 +166,51 @@ audioPlayer.addEventListener('canplaythrough', event => {
 });
 
 audioPlayer.addEventListener('ended', event => {
-  getNextSong();
+  event.preventDefault();
+  if (repeatOneButtonEnabled) {
+    getSameSong();    
+  } else if (randomButtonEnabled) {
+    getRandomSong();
+  } else {
+    getNextSong();
+  }
   audioPlayer.play();
 });
 
 prev.addEventListener('click', event => {
+  event.preventDefault();
   getPrevSong();
   audioPlayer.play();
 });
 
 next.addEventListener('click', event => {
+  event.preventDefault();
   getNextSong();
   audioPlayer.play();
+});
+
+random.addEventListener('click', event => {
+  event.preventDefault();
+  if (randomButtonEnabled) {
+    randomButtonEnabled = false;
+    // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/className
+    random.className = "randomDisabled";
+  } else {
+    randomButtonEnabled = true;
+    random.className = "randomEnabled";
+  }
+});
+
+repeatOne.addEventListener('click', event => {
+  event.preventDefault();
+  if (repeatOneButtonEnabled) {
+    repeatOneButtonEnabled = false;
+    // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/className
+    repeatOne.className = "repeatOneDisabled";
+  } else {
+    repeatOneButtonEnabled = true;
+    repeatOne.className = "repeatOneEnabled";
+  }
 });
 
 function convertSecToMin(seconds) {
@@ -212,11 +257,19 @@ function getNextSong() {
   play.dispatchEvent(clickEvent);
 }
 
+function getSameSong() {
+  currentAudioTime = 0;
+  const clickEvent = new Event('click');
+  audioState = 'paused';
+  play.dispatchEvent(clickEvent);
+}
+
 function getClickedSong(event) {
+  songIdPieces = event.target.id.split('_');
   // Note: the id from the event is actually a string
   // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
   //   /Global_Objects/Number
-  currentSongIndex = Number.parseInt(event.target.id);
+  currentSongIndex = Number.parseInt(songIdPieces[1]);
   const clickEvent = new Event('click');
   audioState = 'paused';
   currentAudioTime = 0;
@@ -225,14 +278,15 @@ function getClickedSong(event) {
 
 function openAlbum(event, albumId) {
   albumListOpen = true;
-  songs = data["albums"][albumId]['songs'];
+  albumIdPieces = albumId.split('_');
+  songs = data["albums"][albumIdPieces[1]]['songs'];
   songList.innerHTML = '<ul></ul>';
   let i = 0;
   songs.forEach(song => {
     let li = document.createElement('li');
     let link = document.createElement('a');
     let linkText = document.createTextNode(song.name);
-    link.id = i;
+    link.id = 'song_' + i;
     link.href = song.url;
     link.className = 'song';
     link.append(linkText);
@@ -244,4 +298,21 @@ function openAlbum(event, albumId) {
     songList.querySelector('ul').append(li);
     i++;
   });
+}
+
+function getRandomSong() {
+  let totalAlbums = data['albums'].length;
+  // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+  //   /Global_Objects/Math/random
+  let randomAlbumId = Math.floor(Math.random() * totalAlbums);
+  let totalSongsOnAlbum = data['albums'][randomAlbumId]['songs'].length;
+  randomSongId = Math.floor(Math.random() * totalSongsOnAlbum);
+  //const clickEvent = new Event('click');
+  /*
+  let focusAlbum = document.getElementById('album_' + randomAlbumId);
+  focusAlbum.dispatchEvent(clickEvent);
+  let focusSong = document.getElementById('song_' + randomSongId);
+  focusSong.dispatchEvent(clickEvent);
+  */
+  return {'albumId': randomAlbumId, 'songId': randomSongId};
 }
