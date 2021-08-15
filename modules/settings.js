5974054490de4583094b734a7926727f05d3d601
summea <summea@gmail.com>
2021-08-14 17:14:49 -0700

    Added modules directory, etc.

---
+++ modules/settings.js
@@ -0,0 +1,6 @@
+// ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+//   /Statements/export
+// ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+export let settings = {
+  "albumsJsonPath": "albums.json"
+};
