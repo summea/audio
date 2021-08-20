b07dd9fa54de7b9df0176f564fc89aac3a4d2ba9
summea <summea@gmail.com>
2021-08-19 22:03:56 -0700

    Began working on adding prev/next album buttons and trying to work on animation

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ let albumListOpen = false;
 let playButton = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"><path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/></svg>';
 let pauseButton = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16"><path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z"/></svg>';
 let randomButton = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-diamond" viewBox="0 0 16 16"><path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/></svg>';
+let nextAlbumButtonSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16"> <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg>';
+let prevAlbumButtonSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16"> <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/></svg>';
 let data = [];
 let currentAudioLoaded = false;
 let randomButtonEnabled = false;
@@ -36,6 +38,8 @@ let prev = document.getElementById("prev");
 let next = document.getElementById("next");
 let random = document.getElementById("random");
 let repeatOne = document.getElementById("repeatOne");
+let nextAlbumButton = document.getElementById("nextAlbumButton");
+let prevAlbumButton = document.getElementById("prevAlbumButton");
 let firstPlay = true;
 let playPromise;
 
@@ -46,6 +50,9 @@ export function setup() {
   // ref: https://developer.mozilla.org/en-US/docs/Web/API
   //   /GlobalEventHandlers/oninput
   audioPlayerSlider.oninput = playbackTimeUpdate;
+
+  prevAlbumButton.innerHTML = prevAlbumButtonSvg;
+  nextAlbumButton.innerHTML = nextAlbumButtonSvg;
 
   // Get song data
   // ref: https://developer.mozilla.org/en-US/docs/Web/API/Request
@@ -442,3 +449,15 @@ function getRandomSong() {
   let randomSongId = Math.floor(Math.random() * totalSongsOnAlbum);
   return {"albumId": randomAlbumId, "songId": randomSongId};
 }
+
+prevAlbumButton.addEventListener('click', function() {
+  let albums = albumList.getElementsByTagName('li');
+  albums[0].className = "animateAlbumsLeft";
+  console.log(albums);
+});
+
+nextAlbumButton.addEventListener('click', function(event) {
+  let albums = albumList.getElementsByTagName('li');
+  albums[0].className = "animateAlbumsRight";
+  console.log(albums);
+});
