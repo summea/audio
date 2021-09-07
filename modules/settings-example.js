e3561f9af383cd2dcab45bac73850320cab24de4
summea <summea@gmail.com>
2021-09-06 21:50:43 -0700

    Added new settings example file and removed old settings file for now

--- modules/settings-example.js
+++ modules/settings-example.js
@@ -2,5 +2,6 @@
 //   /Statements/export
 // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
 export let settings = {
-  "albumsJsonPath": "albums.json"
+  "albumsJsonPath": "albums.json",
+  "backendUrl": "http://127.0.0.1:8000"
 };
