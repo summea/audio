69f26c29b3229049bfd65a9c4d4810b3de6bf1b8
summea <summea@gmail.com>
2021-09-22 22:01:32 -0700

    Broke out `convertSecToMin()` to new module. Closes #92.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide
 //   /Modules
 import { settings } from "./modules/settings.js";
+import { convertSecToMin } from "./modules/utility.js";
 
 // ref: https://developer.mozilla.org/en-US/docs/MDN/Guidelines
 //   /Code_guidelines
@@ -560,23 +561,6 @@ repeatOne.addEventListener("click", event => {
     repeatOne.className = "repeatOneEnabled";
   }
 });
-
-function convertSecToMin(seconds) {
-  let result = {
-    "minutes": 0,
-    "seconds": 0
-  }
-  // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
-  //   /Global_Objects/Math
-  result.minutes = Math.floor(seconds / 60);
-  result.seconds = Math.floor(seconds);
-  if (result.minutes > 0) {
-    // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript
-    //   /Reference/Operators/Remainder
-    result.seconds = Math.floor(Math.round(seconds) % 60);
-  }
-  return result; 
-}
 
 function getPrevSong() {
   currentAudioTime = 0;
